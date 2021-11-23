cb8287c7be993188faf7d941327b6e60e4f2d514
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-11-23 11:24:36 -0500

    Add non-vuforia TeamElementDetector demo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamElementDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamElementDemo.java
@@ -1,83 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.vision.TeamElementDetector;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
 
-/**
- * In this sample, we demonstrate how to use EasyOpenCV in
- * Vuforia passthrough mode. In this mode, EasyOpenCV does not
- * take direct control over the camera. Instead, it pulls frames
- * out of a VuforiaLocalizer's frame queue. This allows you to
- * run both OpenCV and Vuforia simultaneously on the same camera.
- * The downside is that you do not get to choose the resolution
- * of frames delivered to your pipeline, and you do not get any
- * sort of manual control over sensor parameters such as exposure,
- * gain, ISO, or frame rate.
- */
-@TeleOp
-public class TeamElementDemo extends LinearOpMode
-{
-    VuforiaLocalizer vuforia = null;
-    OpenCvCamera vuforiaPassthroughCam;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+
+@Autonomous
+public class TeamElementDemo extends LinearOpMode {
+    OpenCvWebcam webcam;
     TeamElementDetector pipeline;
 
     @Override
     public void runOpMode()
     {
-        /**
-         * NOTE: Many comments have been omitted from this sample for the
-         * sake of conciseness. If you're just starting out with EasyOpenCV,
-         * you should take a look at {@link InternalCamera1Example} or its
-         * webcam counterpart, {@link WebcamExample} first.
-         */
-
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        int[] viewportContainerIds = OpenCvCameraFactory.getInstance().splitLayoutForMultipleViewports(cameraMonitorViewId, 2, OpenCvCameraFactory.ViewportSplitMethod.VERTICALLY);
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        pipeline = new TeamElementDetector();
+        webcam.setPipeline(pipeline);
 
-        /*
-         * Setup Vuforia
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(viewportContainerIds[0]);
-        parameters.vuforiaLicenseKey = "AUmiib//////AAABmbrFAeBqbkd/hmTwBoU6jXFUjeYK8xAgeYu6r9ZuLmpgb4tqNl4oIhXkpbBXmCusnhPlxJ3DHEkExTnQKhvCU49Yu2jslI6vaQ+V5F21ZAbbBod6lm9zyBEpkujo7IOq2TdOaJSIdN5wW3zxrHTksfrzBuKZKRsArompruh7jrm/B4W3F/EunA8ymkVoi29W84q81XMwJyonWlS2sd3pebXvLW0YOKmA63QgdmtSpp9XVAccwiH8ND8rk7FXlIIucim1Ig5FmVPLIx88t7doptXh8uiXfHHMqXc1T1MrRvfemYaUqyg7I5lYLNjLhuRmBZO3BM/qoyjPhpMVtGNh6+z3VgaKhP7O6zI07W0mmMfO";
-        parameters.cameraDirection   = VuforiaLocalizer.CameraDirection.BACK;
-        // Uncomment this line below to use a webcam
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
+        // out when the RC activity is in portrait. We do our actual image processing assuming
+        // landscape orientation, though.
+        webcam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
 
-        // Create a Vuforia passthrough "virtual camera"
-        vuforiaPassthroughCam = OpenCvCameraFactory.getInstance().createVuforiaPassthrough(vuforia, parameters, viewportContainerIds[1]);
-
-        vuforiaPassthroughCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
             {
-                // Using GPU acceleration can be particularly helpful when using Vuforia passthrough
-                // mode, because Vuforia often chooses high resolutions (such as 720p) which can be
-                // very CPU-taxing to rotate in software. GPU acceleration has been observed to cause
-                // issues on some devices, though, so if you experience issues you may wish to disable it.
-                vuforiaPassthroughCam.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
-                vuforiaPassthroughCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
-                vuforiaPassthroughCam.setPipeline(pipeline);
-
-                // We don't get to choose resolution, unfortunately. The width and height parameters
-                // are entirely ignored when using Vuforia passthrough mode. However, they are left
-                // in the method signature to provide interface compatibility with the other types
-                // of cameras.
-                vuforiaPassthroughCam.startStreaming(0,0, OpenCvCameraRotation.UPRIGHT);
+                webcam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -93,12 +52,11 @@ public class TeamElementDemo extends LinearOpMode
 
         while (opModeIsActive())
         {
-            telemetry.addData("Passthrough FPS", vuforiaPassthroughCam.getFps());
-            telemetry.addData("Frame count", vuforiaPassthroughCam.getFrameCount());
-            telemetry.addData("Element position", pipeline.getAnalysis());
+            telemetry.addData("Analysis", pipeline.getAnalysis());
             telemetry.update();
 
-            sleep(100);
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
         }
     }
 }
