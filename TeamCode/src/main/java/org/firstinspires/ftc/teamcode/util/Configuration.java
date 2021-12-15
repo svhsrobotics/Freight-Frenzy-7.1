bc5fa64c01ff443cef0102c7d1f0c2486213f332
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-12-15 15:59:36 -0500

    Use new static config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Configuration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Configuration.java
@@ -1,8 +1,16 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import org.firstinspires.ftc.teamcode.vision.HSVColor;
+import org.firstinspires.ftc.teamcode.vision.Region;
+import org.firstinspires.ftc.teamcode.vision.TeamElementDetector;
+
+import java.util.HashMap;
+
 /**
  * Add all config values to this class
  */
 public class Configuration {
-    public String name;
+    public HashMap<TeamElementDetector.TeamElementPosition, Region> regions;
+    public HSVColor target;
+    public Double threshold;
 }
