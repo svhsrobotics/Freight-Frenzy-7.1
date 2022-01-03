f25475de9ae41ed4c336a5fff3724e3f17f290c3
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2022-01-03 17:04:57 -0500

    chore: add base opmode template to competition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
@@ -1,2 +1,12 @@
-package org.firstinspires.ftc.teamcode;public class CompetitionAuto {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class CompetitionAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
 }
