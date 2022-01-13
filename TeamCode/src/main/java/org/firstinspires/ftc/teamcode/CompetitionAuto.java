d7f27fcb474a625b8f56a2c25ad656994c1064ad
JJTech <jjtech@jjtech.dev>
2022-01-13 15:25:24 -0500

    Refactor comp auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
@@ -4,32 +4,40 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Shared.Drive2;
+import org.firstinspires.ftc.teamcode.Shared.Drive3;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.hardware.Arm;
 import org.firstinspires.ftc.teamcode.util.Configuration;
+import org.firstinspires.ftc.teamcode.util.Configurator;
 import org.firstinspires.ftc.teamcode.vision.HSVColor;
 import org.firstinspires.ftc.teamcode.vision.TeamElementDetector;
 import org.firstinspires.ftc.teamcode.robot.hardware.Webcam;
-import org.opencv.core.Scalar;
 
 @Autonomous
 public class CompetitionAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+
         // Get the webcam from the hardware map
         Webcam webcam = new Webcam("Webcam 1", hardwareMap);
 
-        Scalar target = getTargetColor();
+        Configuration config = Configurator.load();
+        if (config.target == null) {
+            telemetry.log().add("WARNING: WAS NOT CALIBRATED");
+            config.target = new HSVColor(0.0, 0.0, 0.0);
+        }
 
-        TeamElementDetector detector = new TeamElementDetector(target);
+        TeamElementDetector detector = new TeamElementDetector(config);
         webcam.setPipeline(detector);
 
         Robot robot = new Robot(hardwareMap);
         robot.initHardware();
+        robot.arm.reset(); // This resets the encoder to 0
 
         Drive2 drive = new Drive2(robot,this);
 
-        Arm arm = robot.arm;
+
+        waitForStart();
 
         // Open the camera
         webcam.open();
@@ -40,7 +48,7 @@ public class CompetitionAuto extends LinearOpMode {
         TeamElementDetector.TeamElementPosition position = detector.getAnalysis();
         telemetry.addData("Position of the Team Element:", position);
         //Drive away from wall
-        drive.navigationMonitorTicks(1/4, 0, -5, 10);
+        drive.navigationMonitorTicks(1.0/4, 0, -35, 10);
 
         //
         // Position to level mapping:
@@ -53,44 +61,28 @@ public class CompetitionAuto extends LinearOpMode {
         //
         switch (position) {
             case LEFT:
-                arm.toLevel(Arm.HubLevel.Bottom);
+                robot.arm.goToPosition(Arm.HubPosition.BACKBOT);
                 break;
             case CENTER:
-                arm.toLevel(Arm.HubLevel.Middle);
+                robot.arm.goToPosition(Arm.HubPosition.BACKMID);
                 break;
             case RIGHT:
-                arm.toLevel(Arm.HubLevel.Top);
+                robot.arm.goToPosition(Arm.HubPosition.BACKTOP);
                 break;
         }
 
 
         //To Hub
         drive.navigationMonitorTicks(1/2, -5, -5, 10);
-        arm.setCollectorMode(Arm.CollectorMode.Eject);
+        robot.arm.setCollectorMode(Arm.CollectorMode.Eject);
         //Sleeping so the collector has time to eject before stopping the servo
         sleep(2000);
-        arm.setCollectorMode(Arm.CollectorMode.Stop);
+        robot.arm.setCollectorMode(Arm.CollectorMode.Stop);
         //Drive over to Carousel
         drive.navigationMonitorTicks(1, 10, 0,10);
         //TODO: Include Code Using Sensor Data
         telemetry.update();
 
-    }
-
-    public Scalar getTargetColor() {
-        Configuration config = new Configuration();
-        HSVColor hsv;
-
-      if (config.target != null) {
-            hsv = config.target;
-        } else {
-            telemetry.log().add("WARNING WARNING WARNING:");
-            telemetry.log().add("TARGET COLOR WAS NOT CALIBRATED!!!");
-            telemetry.log().add("Please run the Calibrate Target Color OpMode!");
-            android.util.Log.w("TeamElementDemo", "Target Color was NOT CALIBRATED! Falling back to default");
-            hsv = new HSVColor(0.0,0.0,0.0);
-        }
-        return hsv.toScalar();
     }
 
 }
