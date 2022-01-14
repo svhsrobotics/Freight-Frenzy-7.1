4cfc41b9c1a323fa1e9eeaf0d674114043e4797f
JJTech <jjtech@jjtech.dev>
2022-01-13 19:59:16 -0500

    Fix competition auto... again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
@@ -21,36 +21,48 @@ public class CompetitionAuto extends LinearOpMode {
         // Get the webcam from the hardware map
         Webcam webcam = new Webcam("Webcam 1", hardwareMap);
 
+        // Load the configuration
         Configuration config = Configurator.load();
         if (config.target == null) {
+            // This is an extra warning to make debugging errors a little easier
             telemetry.log().add("WARNING: WAS NOT CALIBRATED");
+            // So we don't get a NullPointerException later on...
             config.target = new HSVColor(0.0, 0.0, 0.0);
         }
 
+        // Create a detector pipeline from the config
         TeamElementDetector detector = new TeamElementDetector(config);
+        // Tell the camera to start using the pipeline
         webcam.setPipeline(detector);
-
+        // Create the robot from the hardware map
         Robot robot = new Robot(hardwareMap);
+        // Initialize the hardware
         robot.initHardware();
-        robot.arm.reset(); // This resets the encoder to 0
-
+        // Reset the arm encoder to 0 to prevent some issues...
+        robot.arm.reset();
+        // Create a drive. Note that passing the entire OpMode is not ideal, should be fixed later
         Drive2 drive = new Drive2(robot,this);
 
-
-        waitForStart();
-
-        // Open the camera
+        // Open the camera; also begins streaming the pipeline
         webcam.open();
 
         // Wait for the OpMode to start
+        // Make sure to do this after the camera is opened; otherwise "View Camera Stream" won't work
         waitForStart();
 
+        // As soon as we start, get the position
         TeamElementDetector.TeamElementPosition position = detector.getAnalysis();
-        telemetry.addData("Position of the Team Element:", position);
-        //Drive away from wall
-        drive.navigationMonitorTicks(1.0/4, 0, -35, 10);
+        // Print it to the telemetry
+        telemetry.log().add("Position of the Team Element: " + position); telemetry.update();
 
-        //
+        // Drive away from wall so the arm doesn't hit it.
+        drive.navigationMonitorTicks(1.0/4, 0, -10, 10);
+        // Raise the arm so it doesn't drag.
+        robot.arm.setPositions(-1300, .75);
+
+        // Set the target angle (rotate to that angle). Note that this is in radians
+        drive.setTargetAngle(0);
+
         // Position to level mapping:
         //        []
         //    \___||___/  <-- RIGHT
@@ -59,10 +71,13 @@ public class CompetitionAuto extends LinearOpMode {
         //        ||
         //  \_____||_____/  <-- LEFT
         //
+
+        /*
+        // Position the arm to the correct level
         switch (position) {
             case LEFT:
                 robot.arm.goToPosition(Arm.HubPosition.BACKBOT);
-                break;
+                break; // Break is *very* important
             case CENTER:
                 robot.arm.goToPosition(Arm.HubPosition.BACKMID);
                 break;
@@ -70,19 +85,27 @@ public class CompetitionAuto extends LinearOpMode {
                 robot.arm.goToPosition(Arm.HubPosition.BACKTOP);
                 break;
         }
+        */
 
 
-        //To Hub
-        drive.navigationMonitorTicks(1/2, -5, 0, 10);
+        // Drive to the alliance hub
+        drive.navigationMonitorTicks(1.0/2, -5, 0, 10);
+        // Start ejecting the block
         robot.arm.setCollectorMode(Arm.CollectorMode.Eject);
-        //Sleeping so the collector has time to eject before stopping the servo
+        // Wait for it to eject (should tune this amount...)
         sleep(2000);
+        // Ok stop ejecting now
         robot.arm.setCollectorMode(Arm.CollectorMode.Stop);
-        //Drive over to Carousel
-        drive.navigationMonitorTicks(1, 10, 0,10);
-        //TODO: Include Code Using Sensor Data
-        telemetry.update();
 
+        // Drive to the carousel
+        drive.navigationMonitorTicks(1.0, 10, 0,10);
+
+        // IDK what else needs to happen. Something about positioning for the carousel
+
+        // Now we need to park, right?
+
+        // Just wait for the OpMode to end... Very important so that things continue running lol.
+        while (opModeIsActive()) {}
     }
 
 }
